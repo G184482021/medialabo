7149c66a6a825198047e23f9b654a644f55f2e0b
G184482021 <218448@st.takushoku-u.ac.jp>
2022-07-21 15:29:53 +0900

    7/21

--- weather.js
+++ weather.js
@@ -1,3 +1,4 @@
+/*
 let data = {
   "coord": {
     "lon": 116.3972,
@@ -47,3 +48,53 @@ let data = {
 
 ////////// 課題3-2 ここからプログラムを書こう
 console.log(data.name + ', ' + data.main.temp_max + ', ' + data.main.temp_min);
+*/
+
+let b = document.querySelector('#sendRequest');
+b.addEventListener('click', sendRequest);
+
+function sendRequest() {
+  let id = document.getElementById("town-select").value;
+	let url = 'https://www.nishita-lab.org/web-contents/jsons/openweather/' + id +'.json';
+  
+	axios.get(url)
+		.then(showResult)
+		.catch(showError)
+		.then(finish);
+}
+
+function showResult(resp) {
+	let data = resp.data;
+  let pn = document.querySelector('span#name');
+  let pw = document.querySelector('span#weather_description');
+  let pma = document.querySelector('span#temp_max');
+  let pmi = document.querySelector('span#temp_min');
+  let ph = document.querySelector('span#humidity');
+  let pws = document.querySelector('span#wind_speed');
+  let pwd = document.querySelector('span#wind_deg');
+  let pclo = document.querySelector('span#coord_lon');
+  let pcla = document.querySelector('span#coord_lat');
+  pn.textContent = '都市名：' + data.name;
+  pw.textContent = '天気：' + data.weather[0].description;
+  pma.textContent = '最高気温：' + data.main.temp_max;
+  pmi.textContent = '最低気温：' + data.main.temp_min;
+  ph.textContent = '湿度：' + data.main.humidity;
+  pws.textContent = '風速：' + data.wind.speed;
+  pwd.textContent = '風向：' + data.wind.deg;
+  pclo.textContent = '緯度：' + data.coord.lon;
+  pcla.textContent = '経度：' + data.coord.lat;
+
+	if (typeof data === 'string') {
+		data = JSON.parse(data);
+	}
+
+	console.log(data);
+}
+
+function showError(err) {
+	console.log(err);
+}	
+
+function finish() {
+	console.log('Ajax 通信が終わりました');
+}
